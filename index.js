b00af62341f34c1e13347d6491d76e5f58f9d002
pietrocosentino88@gmail.com <pietrocosentino88@gmail.com>
2024-03-15 00:52:09 -0300

    segundo desafio

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("olá mundo")
+let resultado = rankeadas(50, 17)
+
+console.log(resultado)
+
+function rankeadas(vitorias, derrotas) {
+    let pontuacao = vitorias - derrotas
+    {
+        if (pontuacao > 10) {
+            console.log("Você está no ferro")
+        } else if ((pontuacao >= 11) && (pontuacao <= 20)) {
+            console.log("Você está no bronze")
+        } else if ((pontuacao >= 21) && (pontuacao <= 50)) {
+            console.log("Você está no prata")
+        } else if ((pontuacao >= 51) && (pontuacao <= 80)) {
+            console.log("Você está no ouro")
+        } else if ((pontuacao >= 81) && (pontuacao <= 90)) {
+            console.log("Você está no diamante")
+        } else if ((pontuacao >= 91) && (pontuacao <= 100)) {
+            console.log("Você está no lendário")
+        } else if (pontuacao >= 101) {
+            console.log("Você está no imortal")
+        }
+
+        return pontuacao;
+
+    }
+}
